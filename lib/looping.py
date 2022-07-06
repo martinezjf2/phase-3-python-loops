bb250bbd535eaacfd7e1c0f102178e03c8f65568
Jeffrey Martinez <martinezjf2@gmail.com>
2022-07-06 14:22:38 -0400

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here
-    pass
+    i = 10
+    while (i > 0):
+        print(i)
+        i = i -1
+    print("Happy New Year!")
+    
 
 def reverse_string(string):
     # code goes here
-    pass
+    a = ''
+    for i in string:
+        a = i + a
+    return a
 
 def fizzbuzz():
     # code goes here
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            continue
+        elif i % 3 == 0:
+            print("Fizz")
+            continue
+        elif i % 5 == 0:
+            print("Buzz")
+            continue
+       
+        print(str(i))
+    # pass
